399316ad586741045621013a67584eadd9702e70
Rhixin <vallestedted@gmail.com>
2025-03-17 15:00:41 +0800

    [Zhazted] Added Interpreter class and RuntimeError class

--- src/bisaya/Bisaya.java
+++ src/bisaya/Bisaya.java
@@ -9,7 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Bisaya {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -29,6 +31,10 @@ public class Bisaya {
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
+
+        // Indicate an error in the exit code.
+        if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -51,6 +57,7 @@ public class Bisaya {
             System.exit(65);
         }
 
+        //TOKENIZATION
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
@@ -60,7 +67,10 @@ public class Bisaya {
         // Stop if there was a syntax error.
         if (hadError) return;
 
+        //PARSING
+        //print expression before parsing. just to check lang
         System.out.println(new AstPrinter().print(expression));
+        interpreter.interpret(expression);
     }
 
     //Suggestion:
@@ -74,5 +84,10 @@ public class Bisaya {
     private static void report(int line, String where, String message){
         System.err.println("[line " + line + "] Error " + where + ": " + message);
         hadError = true;
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 }
